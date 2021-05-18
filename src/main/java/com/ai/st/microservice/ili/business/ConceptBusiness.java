a159d815fc6786f096375ef25656ff7cecd8cd5d
shiido <jhonfr29@gmail.com>
2021-05-18 12:52:11 -0500

    feat: Add support to handle 1.0 version BPM

--- src/main/java/com/ai/st/microservice/ili/business/ConceptBusiness.java
+++ src/main/java/com/ai/st/microservice/ili/business/ConceptBusiness.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class ConceptBusiness {
 
-	public static final Long CONCEPT_OPERATION = (long) 1;
+    public static final Long CONCEPT_OPERATION = (long) 1;
 
-	public static final Long CONCEPT_INTEGRATION = (long) 2;
+    public static final Long CONCEPT_INTEGRATION = (long) 2;
+
+    public static final Long CONCEPT_RECEIPT_FROM_BPM = (long) 3;
 
 }
